0f674603249be3010cc6cd73ad974a414a02c6c9
ghostkill73 <73VxteewH4JF@proton.me>
2024-07-06 18:23:34 -0300

    adding checksum function

--- shcrypt.sh
+++ shcrypt.sh
@@ -40,7 +40,7 @@ _shcrypt_msg()
 # if an option does not exist, print message error
 _shcrypt_invalid_option()
 {
-	__invalid__=''
+	__invalid__=
 	unset __invalid__
 	: ${__invalid__?"$1"}
 	exit ${_shcrypt_error}
@@ -55,7 +55,7 @@ _shcrypt_text_test()
 		unset _invalid_text
 		: ${_invalid_text?"$1"}
 		exit ${_shcrypt_false}
-	} #end
+	} #endif
 }
 
 # file test
@@ -67,7 +67,7 @@ _shcrypt_file_test()
 		unset _invalid_file
 		: ${_invalid_file?"$1"}
 		exit ${_shcrypt_false}
-	} #end
+	} #endif
 }
 
 
@@ -91,7 +91,7 @@ _shcrypt_md5()
 {
 	_md5_opt="$1"
 	_md5_in="$2"
-	_md5_out=''
+	_md5_out=
 
 	case "$_md5_opt" in
 
@@ -99,7 +99,7 @@ _shcrypt_md5()
 			_shcrypt_text_test "$_md5_in"
 
 			_md5_out="`printf "$_md5_in" | md5sum`"
-			_shcrypt_msg "${_md5_out%%% *}"
+			_shcrypt_msg "${_md5_out%% *}"
 		;;
 
 		-f|--file)
@@ -124,7 +124,7 @@ _shcrypt_sha1()
 {
 	_sha1_opt="$1"
         _sha1_in="$2"
-        _sha1_out=''
+        _sha1_out=
 
         case "$_sha1_opt" in
 
@@ -157,7 +157,7 @@ _shcrypt_sha224()
 {
         _sha224_opt="$1"
         _sha224_in="$2"
-        _sha224_out=''
+        _sha224_out=
 
         case "$_sha224_opt" in
 
@@ -190,7 +190,7 @@ _shcrypt_sha256()
 {
         _sha256_opt="$1"
 	_sha256_in="$2"
-        _sha256_out=''
+        _sha256_out=
 
         case "$_sha256_opt" in
 
@@ -224,7 +224,7 @@ _shcrypt_sha384()
 {
         _sha384_opt="$1"
 	_sha384_in="$2"
-        _sha384_out=''
+        _sha384_out=
 
         case "$_sha384_opt" in
 
@@ -258,7 +258,7 @@ _shcrypt_sha512()
 {
         _sha512_opt="$1"
 	_sha512_in="$2"
-        _sha512_out=''
+        _sha512_out=
 
         case "$_sha512_opt" in
 
@@ -291,7 +291,7 @@ _shcrypt_b2()
 {
         _b2_opt="$1"
 	_b2_in="$2"
-        _b2_out=''
+        _b2_out=
 
         case "$_b2_opt" in
 
@@ -319,6 +319,40 @@ _shcrypt_b2()
 }
 
 
+############################################################
+# CHECKSUM utils
+# Avaliable functions:
+#	- hash checksum
+#
+############################################################
+
+_shcrypt_checksum()
+{
+	_ck_opt="$1"
+	_ck_file1="$2"
+	_ck_file2="$3"
+
+	case "$_ck_opt" in
+
+		--md5|--sha1|--sha224|--sha256|--sha384|--sha512|--b2)
+			_ck_file1_hash="`printf "$_ck_file1" | "${_ck_opt#--}sum"`"
+			_ck_file2_hash="`printf "$_ck_file2" | "${_ck_opt#--}sum"`"
+
+			_shcrypt_file_test "$_ck_file1"
+			_shcrypt_file_test "$_ck_file2"
+
+			[ "${_ck_file1_hash}" = "${_ck_file2_hash}" ] && { #if
+				return ${_shcrypt_true}
+			} || { #else
+				return ${_shcrypt_false}
+			} #endif
+		;;
+
+		*)
+			_shcrypt_invalid_option "$_ck_opt"
+		;;
+	esac
+}
 
 ############################################################
 # GENERATORS utils
@@ -333,7 +367,7 @@ _shcrypt_b2()
 # _shcrypt_uuid
 _shcrypt_uuid()
 {
-	N="1"
+	N="0"
 	C="89ab"
 
 	while [ "$N" -lt "16" ]; do
@@ -368,9 +402,7 @@ _shcrypt_uuid()
 	return ${_shcrypt_true}
 }
 
-## much faster uuid generator alternative, but not all
-## systems will support it
-
+## much faster uuid generator alternative
 # _shcrypt_uuid2
 _shcrypt_uuid2()
 {
@@ -384,9 +416,9 @@ _shcrypt_rnumber()
 {
 	_rnum_in="$1"
 	_rnum_length="$2"
-	_rnum_out=''
+	_rnum_out=
 
-	: ${_rnum_length:=10}
+	: ${_rnum_length:=100}
 
 	_rnum_out="`LC_ALL=C head -n "$_rnum_length" /dev/urandom | tr -dc '0-9' 2>/dev/null | head -c "$_rnum_in"`"
 
@@ -400,9 +432,9 @@ _shcrypt_rtext()
 {
 	_rtext_in="$1"
 	_rtext_length="$2"
-	_rtext_out=''
+	_rtext_out=
 
-	: ${_rtext_length:=10}
+	: ${_rtext_length:=100}
 
 	_rtext_out="`LC_ALL=C head -n "$_rtext_length" /dev/urandom | tr -dc 'a-zA-Z' 2>/dev/null | head -c "$_rtext_in"`"
 
@@ -416,9 +448,9 @@ _shcrypt_random()
 {
 	_rtext_in="$1"
 	_rtext_length="$2"
-	_rtext_out=''
+	_rtext_out=
 
-	: ${_rtext_length:=10}
+	: ${_rtext_length:=100}
 
 	_rtext_out="`LC_ALL=C head -n "$_rtext_length" /dev/urandom | tr -dc '[:graph:]' 2>/dev/null | head -c "$_rtext_in"`"
 
